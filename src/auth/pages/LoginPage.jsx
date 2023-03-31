da64e7d48d840174288cef2f3549de76c08aca75
Fernando Andres Poblete <andrespbt14@hotmail.com>
2023-03-31 17:36:38 -0300

    fix: create account input fixed

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -142,7 +142,7 @@ export const LoginPage = () => {
               <input
                 type="submit"
                 className="btnSubmit"
-                value="Crear cuenta"
+                value="Create account"
               />
             </div>
           </form>
